fb092d5e9c326acd4c98f40a698126ad63a6774c
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2025-11-11 21:50:29 -0300

    :sparkles: feat: Post is working, need to solve some bugs tho

--- src/main/java/com/feelytics/Main.java
+++ src/main/java/com/feelytics/Main.java
@@ -1,9 +1,13 @@
 package com.feelytics;
 
+import com.feelytics.data.collection.APICaller;
+
 public class Main {
     public static void main(String[] args) {
 
+        APICaller caller = new APICaller();
 
+        caller.getApiData("Something");
 
     }
 }
